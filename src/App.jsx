ed975b4164b1020b60075cc9e6f28bbc6a0797ed
kaushal <kausraj11@gmail.com>
2024-11-06 16:18:15 +0530

    kaushal

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RoomList from "./Pages/RoomList";
 import RoomDetail from "./Pages/RoomDetails";
 function App() {
   return (
     <ChakraProvider value={defaultSystem}>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<RoomList />} />
           <Route path="/room/:id" element={<RoomDetail />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
