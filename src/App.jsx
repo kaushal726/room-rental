7966da8ed88b2d8fac784349c870348e3ddbad05
kaushal <kausraj11@gmail.com>
2024-11-26 15:08:12 +0530

    added analystics

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RoomList from "./Pages/RoomList";
 import RoomDetail from "./Pages/RoomDetails";
+import ReactGA from "react-ga";
+
 const App = () => {
+  ReactGA.initialize("370077440");
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <ChakraProvider value={defaultSystem}>
       <BrowserRouter>
